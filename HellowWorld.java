3636cb6fd5fc49cba7a2df71ab16e04da59a9c62
somendra <somendra>
2020-04-24 07:04:24 +0530

    new changes

---
+++ HellowWorld.java
@@ -0,0 +1,11 @@
+package com.myWork.BasicPrograms;
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		System.out.println("Have a Good Day");
+		System.out.println("Have a Good Day for all");
+	}
+
+}
